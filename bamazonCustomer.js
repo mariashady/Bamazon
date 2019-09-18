6cf23038069e6138bd95daac0774f5d5291e24d2
Maria Shady <maria.shady@yahoo.com>
2019-09-18 17:57:39 -0400

    data from products table pulling up in terminal

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,28 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const consoleTable = require("console.table");
+// const consoleTable = require("console.table");
+const table = require("cli-table");
+
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+
+    user: "root",
+    password: "ilovelamp1",
+    database: "Bamazon"
+});
+
+connection.connect(function(err) {
+    if(err) throw err;
+});
+
+let display = function() {
+    connection.query("SELECT * FROM products", function(err, results) {
+        if (err) throw err;
+        console.log(results);
+    })
+};
+
+display();
+
+
