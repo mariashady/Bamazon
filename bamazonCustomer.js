7d064848798f13f806996963675f061a048edcbd
Maria Shady <maria.shady@yahoo.com>
2019-09-20 16:51:07 -0400

    shows total for customer, updates stock quantity in database, re-directs customer if there is not enough in stock for their order

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,8 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
-// const table = require("cli-table");
+
+
 
 let connection = mysql.createConnection({
     host: "localhost",
@@ -12,16 +13,64 @@ let connection = mysql.createConnection({
     database: "Bamazon"
 });
 
-connection.connect(function(err) {
-    if(err) throw err;
+connection.connect(function (err) {
+    if (err) throw err;
 });
 
-let display = function() {
-    connection.query("SELECT * FROM products", function(err, results) {
+let display = function () {
+    connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
         console.table(results);
+        start();
     })
 };
+
+function start() {
+    inquirer
+        .prompt([{
+
+            name: "product",
+            type: "input",
+            message: "What is the item_id of the product you would like to purchase?"
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "How many?"
+
+        }
+    ]) .then(function(answer){
+        connection.query("SELECT * FROM products WHERE ?",
+        {item_id:answer.product}, function (err, res) {
+            if (err) throw err;
+            if (answer.quantity > res[0].stock_quantity) {
+                inquirer
+                    .prompt([
+                        {
+
+                           name: "continue",
+                           type: "input",
+                           message: "Out of Stock. Would you like to place another order?",
+           
+                        }
+                    ]).then(function(answer) {
+                        if(answer.continue == "yes" || answer.continue == "y") {
+                            start()
+                        }
+                    })
+            }else {
+                connection.query("UPDATE products SET ? WHERE ? ", [{stock_quantity:res[0].stock_quantity - answer.quantity}, {item_id:answer.product}], function (err, updateRes) {
+                    if (err) throw err;
+                    console.log("Total: ", res[0].price * answer.quantity);
+                })
+            }
+            
+        })
+    })
+}
+
+
+
 
 
 
